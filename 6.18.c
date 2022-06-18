2ffc761f33ff1d09ac6b12c90e1c51dcf330316a
CoderSundh <107179789+CoderSundh@users.noreply.github.com>
2022-06-18 11:30:45 +0800

    Add files via upload
    存储类别学习

---
+++ 6.18.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int fun(int b);
+int main()
+{
+	int a = 10;
+	int c = 0;
+	//c=fun(a);
+	fun(a);
+	c = c + fun(a);
+	printf("%d\n",a);
+	printf("%d\n", c);
+	return 0;
+}
+int fun(int b)
+{
+	b = 0;
+	//return b;
+	//return 2;
+}
