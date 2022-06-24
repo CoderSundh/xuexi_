db75caeb06179603f9842481b9835e83de7c7eb3
CoderSundh <107179789+CoderSundh@users.noreply.github.com>
2022-06-24 14:00:50 +0800

    Add files via upload

---
+++ 6.24.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+//DIY库函数 DIY strcpy
+
+//方法1：
+char* myStrcpy1(char* pDes, const char* pSrc)
+//const 的意思是用const定义的变量的值是不允许改变的，不允许给他重新赋值，防止在这个函数中传来的数值被改变
+{
+	if(0==pDes||0==pSrc){
+		return 0;
+	}
+	char* t = pDes;
+	while (*pDes != '\0')
+	{
+		*pDes++ = *pSrc++;
+	}
+	*pDes = '\0';
+	return t;
+}
+int	main(void)
+{
+	char a[100] = "china";
+	char b[100] = "japan";
+	char t[100] = "     ";
+	myStrcpy1(t, a);
+	myStrcpy1(a, b);
+	myStrcpy1(b, t);
+	puts(a);
+	puts(b);
+	return 0;
+}
