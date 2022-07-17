ac08ce25c3ca2b24f439f85ec5e85f0ecb58d29b
CoderSundh <107179789+CoderSundh@users.noreply.github.com>
2022-07-17 17:13:20 +0800

    Add files via upload

---
+++ 7.17.c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+/*
+
+#include <stdio.h>
+
+int main()
+{
+    printf("你好！\n");
+
+    return 0;
+}
+*/
+/*
+#include <stdio.h>
+
+int main()
+{
+    printf("23+43=%d\n", 23 + 43);
+
+    return 0;
+}
+*/
+/*
+#include <stdio.h>
+
+int main()
+{
+    int price = 0;
+
+    printf("请输入金额（元）：");
+    scanf("%d", &price);
+
+    int change = 100 - price;
+
+    printf("找您%d元。\n", change);
+
+    return 0;
+}
+*/
+
+#include <stdio.h>
+int main()
+{
+    int i=0, j = 0;
+    //i = j + 2;
+    printf("%d\n",i);
+    return 0;
+}
