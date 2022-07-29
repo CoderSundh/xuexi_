646ba84fdcfe11fa6b4a0038fd8dd3e0f51d4248
CoderSundh <107179789+CoderSundh@users.noreply.github.com>
2022-07-29 19:41:40 +0800

    Add files via upload

---
+++ 7.29.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a, b;
+//
+//	printf("请输入两个整数：");
+//	scanf("%d %d", &a, &b);
+//
+//	int max = b;
+//	if (a > b) {
+//		max = a;
+//	}
+//
+//	printf("大的那个是%d\n", max);
+//
+//	return 0;
+//} 
+
+
+/*
+int main()
+{
+	int a = 6;
+	int b = 7;
+	if (a == 7)
+		if (b == 6)
+			printf("A");
+		else
+			printf("B");
+	return 0;
+}
+*/
+
+
